c1efc4e9a93d8c78e1a9014ae905d1904923dccb
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-08-13 16:47:25 +0700

    add base project

---
+++ app/src/main/java/me/dimasmiftah/tapmatch/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.dimasmiftah.tapmatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
