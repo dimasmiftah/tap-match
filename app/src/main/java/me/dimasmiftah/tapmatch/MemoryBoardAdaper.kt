3e468dc1969ed2f911b318f6174844bb82dd5a21
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-08-17 17:29:43 +0700

    style updates

--- app/src/main/java/me/dimasmiftah/tapmatch/MemoryBoardAdaper.kt
+++ app/src/main/java/me/dimasmiftah/tapmatch/MemoryBoardAdaper.kt
@@ -61,7 +61,7 @@ class MemoryBoardAdaper(
                     imageButton.setImageResource(memoryCard.identifier)
                 }
             } else {
-                imageButton.setImageResource(R.drawable.ic_launcher_background)
+                imageButton.setImageResource(R.drawable.bamboo)
             }
 
             imageButton.alpha = if (memoryCard.isMatched) .4f else 1.0f
